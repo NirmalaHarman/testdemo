f723fcb037a715f7040626604d4d1d0aac06d7ac
NirmalaHarman <mnanda85@gmail.com>
2022-12-16 17:12:41 +0530

    Test Demo

---
+++ src/THRead/Thread1.java
@@ -0,0 +1,13 @@
+package THRead;
+
+import java.io.*;
+
+public class Thread1
+{
+	public static void main(String[] args)
+	{
+		
+		System.out.println(" okayyy");
+	}
+}
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NChinnasamy
+ *
+ */
+module MYFirstThread {
+}
